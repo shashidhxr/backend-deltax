2fe8d61c06404fae56fd977b9af8b9997623dcb1
Shashidhar Biradar <shashidharb459@gmail.com>
2025-03-18 10:51:39 +0530

    deployment config update

--- config/db.js
+++ config/db.js
@@ -4,6 +4,8 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+// const fs = require('fs');
+
 const pool = new Pool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
@@ -11,8 +13,8 @@ const pool = new Pool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     ssl: {
-        rejectUnauthorized: process.env.NODE_ENV === 'production'
-    }
+        rejectUnauthorized: false // Temporary fix for self-signed certs
+      }
 });
 
 pool.connect((err, client, release) => {

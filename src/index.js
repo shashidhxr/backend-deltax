d63e48b1f27bbd647857a5a7e187092606d27060
Shashidhar Biradar <shashidharb459@gmail.com>
2025-03-22 22:53:49 +0530

    trial and error

--- src/index.js
+++ src/index.js
@@ -11,8 +11,7 @@ const app = express()
 
 app.use(
     cors({
-        // origin: `${process.env.FRONTEND_URL}`,
-        origin: 'https://deltax0.vercel.app/',
+        origin: `${process.env.FRONTEND_URL}`,
         credentials: true,
     })
 );
